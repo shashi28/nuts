0b27699dc479dc2f0d088caae7df620240174b3f
shashi28 <cursedshashi@gmail.com>
2014-04-18 00:43:41 +0530

    edit Packet Analyzer

--- Packet Analyzer/packSniff.py
+++ Packet Analyzer/packSniff.py
@@ -9,155 +9,191 @@ http://oss.coresecurity.com/projects/pcapy.html
 
 import socket
 from struct import *
-import datetime
 import pcapy
-import sys
+from PyQt4.QtCore import *
 
-def main(argv):
-    #list all devices
-    devices = pcapy.findalldevs()
-    print devices
+class Sniffer(QThread):
+    def main(self,dev):
+        #list all devices
+        qDebug('inside main')
+        qDebug("Sniffing device " + dev)
 
-    #ask user to enter device name to sniff
-    print "Available devices are :"
-    for d in devices :
-        print d
+        '''
+        open device
+        # Arguments here are:
+        #   device
+        #   snaplen (maximum number of bytes to capture _per_packet_)
+        #   promiscious mode (1 for true)
+        #   timeout (in milliseconds)
+        '''
+        cap = pcapy.open_live(dev , 65536 , 1 , 0)
 
-    dev = raw_input("Enter device name to sniff : ")
+        #start sniffing packets
+        while(True):
+            qDebug('inside while(1)')
 
-    print "Sniffing device " + dev
+            (header, packet) = cap.next()
+            #print ('%s: captured %d bytes, truncated to %d bytes' %(datetime.datetime.now(), header.getlen(), header.getcaplen()))
+            self.parse_packet(packet)
 
-    '''
-    open device
-    # Arguments here are:
-    #   device
-    #   snaplen (maximum number of bytes to capture _per_packet_)
-    #   promiscious mode (1 for true)
-    #   timeout (in milliseconds)
-    '''
-    cap = pcapy.open_live(dev , 65536 , 1 , 0)
+    #Convert a string of 6 characters of ethernet address into a dash separated hex string
+    def eth_addr (self,a) :
+        b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
+        return b
 
-    #start sniffing packets
-    while(1) :
-        (header, packet) = cap.next()
-        #print ('%s: captured %d bytes, truncated to %d bytes' %(datetime.datetime.now(), header.getlen(), header.getcaplen()))
-        parse_packet(packet)
+    #function to parse a packet
+    def parse_packet(self,packet) :
 
-#Convert a string of 6 characters of ethernet address into a dash separated hex string
-def eth_addr (a) :
-    b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
-    return b
+        qDebug('inside parse_packet')
 
-#function to parse a packet
-def parse_packet(packet) :
+        #parse ethernet header
+        eth_length = 14
 
-    #parse ethernet header
-    eth_length = 14
+        eth_header = packet[:eth_length]
+        eth = unpack('!6s6sH' , eth_header)
+        eth_protocol = socket.ntohs(eth[2])
 
-    eth_header = packet[:eth_length]
-    eth = unpack('!6s6sH' , eth_header)
-    eth_protocol = socket.ntohs(eth[2])
-    print 'Destination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(packet[6:12]) + ' Protocol : ' + str(eth_protocol)
+        print 'Destination MAC : ' + self.eth_addr(packet[0:6]) + ' Source MAC : ' + self.eth_addr(packet[6:12]) + ' Protocol : ' + str(eth_protocol)
 
-    #Parse IP packets, IP Protocol number = 8
-    if eth_protocol == 8 :
-        #Parse IP header
-        #take first 20 characters for the ip header
-        ip_header = packet[eth_length:20+eth_length]
+        D_MAC = self.eth_addr(packet[0:6])
+        S_MAC = self.eth_addr(packet[6:12])
+        Pro = str(eth_protocol)
 
-        #now unpack them :)
-        iph = unpack('!BBHHHBBH4s4s' , ip_header)
-
-        version_ihl = iph[0]
-        version = version_ihl >> 4
-        ihl = version_ihl & 0xF
-
-        iph_length = ihl * 4
-
-        ttl = iph[5]
-        protocol = iph[6]
-        s_addr = socket.inet_ntoa(iph[8]);
-        d_addr = socket.inet_ntoa(iph[9]);
-
-        print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
-
-        #TCP protocol
-        if protocol == 6 :
-            t = iph_length + eth_length
-            tcp_header = packet[t:t+20]
+        self.emit(SIGNAL('MAC(QString,QString,QString)'),D_MAC,S_MAC,Pro)
+        #Parse IP packets, IP Protocol number = 8
+        if eth_protocol == 8 :
+            #Parse IP header
+            #take first 20 characters for the ip header
+            ip_header = packet[eth_length:20+eth_length]
 
             #now unpack them :)
-            tcph = unpack('!HHLLBBHHH' , tcp_header)
+            iph = unpack('!BBHHHBBH4s4s' , ip_header)
 
-            source_port = tcph[0]
-            dest_port = tcph[1]
-            sequence = tcph[2]
-            acknowledgement = tcph[3]
-            doff_reserved = tcph[4]
-            tcph_length = doff_reserved >> 4
+            version_ihl = iph[0]
+            version = version_ihl >> 4
+            ihl = version_ihl & 0xF
 
-            print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
+            iph_length = ihl * 4
 
-            h_size = eth_length + iph_length + tcph_length * 4
-            data_size = len(packet) - h_size
+            ttl = iph[5]
+            protocol = iph[6]
+            s_addr = socket.inet_ntoa(iph[8]);
+            d_addr = socket.inet_ntoa(iph[9]);
 
-            #get data from the packet
-            data = packet[h_size:]
+            print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
 
-            print 'Data : ' + data
+            Version = str(version)
+            IP_Header_Length = str(ihl)
+            TTL =  str(ttl)
+            Protocol = str(protocol)
+            Source_Address = str(s_addr)
+            Destination_Address =  str(d_addr)
 
-        #ICMP Packets
-        elif protocol == 1 :
-            u = iph_length + eth_length
-            icmph_length = 4
-            icmp_header = packet[u:u+4]
+            self.emit(SIGNAL('IP(QString,QString,QString,QString,QString,QString)'),Version,IP_Header_Length,TTL,Protocol,Source_Address,Destination_Address)
 
-            #now unpack them :)
-            icmph = unpack('!BBH' , icmp_header)
+            #TCP protocol
+            if protocol == 6 :
+                t = iph_length + eth_length
+                tcp_header = packet[t:t+20]
 
-            icmp_type = icmph[0]
-            code = icmph[1]
-            checksum = icmph[2]
+                #now unpack them :)
+                tcph = unpack('!HHLLBBHHH' , tcp_header)
 
-            print 'Type : ' + str(icmp_type) + ' Code : ' + str(code) + ' Checksum : ' + str(checksum)
+                source_port = tcph[0]
+                dest_port = tcph[1]
+                sequence = tcph[2]
+                acknowledgement = tcph[3]
+                doff_reserved = tcph[4]
+                tcph_length = doff_reserved >> 4
 
-            h_size = eth_length + iph_length + icmph_length
-            data_size = len(packet) - h_size
+                print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
 
-            #get data from the packet
-            data = packet[h_size:]
+                Source_Port = str(source_port)
+                Dest_Port = str(dest_port)
+                Sequence_Number = str(sequence)
+                Acknowledgement = str(acknowledgement)
+                TCP_header_length = str(tcph_length)
 
-            print 'Data : ' + data
+                self.emit(SIGNAL('TCP(QString,QString,QString,QString,QString)'),Source_Port,Dest_Port,Sequence_Number,Acknowledgement,TCP_header_length)
 
-        #UDP packets
-        elif protocol == 17 :
-            u = iph_length + eth_length
-            udph_length = 8
-            udp_header = packet[u:u+8]
+                h_size = eth_length + iph_length + tcph_length * 4
+                data_size = len(packet) - h_size
 
-            #now unpack them :)
-            udph = unpack('!HHHH' , udp_header)
+                #get data from the packet
+                data = packet[h_size:]
 
-            source_port = udph[0]
-            dest_port = udph[1]
-            length = udph[2]
-            checksum = udph[3]
+                print 'Data : ' + data
 
-            print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Length : ' + str(length) + ' Checksum : ' + str(checksum)
+                self.emit(SIGNAL('Data(QString)'),data)
+            #ICMP Packets
+            elif protocol == 1 :
+                u = iph_length + eth_length
+                icmph_length = 4
+                icmp_header = packet[u:u+4]
 
-            h_size = eth_length + iph_length + udph_length
-            data_size = len(packet) - h_size
+                #now unpack them :)
+                icmph = unpack('!BBH' , icmp_header)
 
-            #get data from the packet
-            data = packet[h_size:]
+                icmp_type = icmph[0]
+                code = icmph[1]
+                checksum = icmph[2]
 
-            print 'Data : ' + data
+                print 'Type : ' + str(icmp_type) + ' Code : ' + str(code) + ' Checksum : ' + str(checksum)
 
-        #some other IP packet like IGMP
-        else :
-            print 'Protocol other than TCP/UDP/ICMP'
+                Type = str(icmp_type)
+                Code = str(code)
+                Checksum = str(checksum)
+                self.emit(SIGNAL('ICMP(QString,QString,QString)'),Type,Code,Checksum)
 
-        print
+                h_size = eth_length + iph_length + icmph_length
+                data_size = len(packet) - h_size
 
-if __name__ == "__main__":
-  main(sys.argv)
+                #get data from the packet
+                data = packet[h_size:]
+
+                print 'Data : ' + data
+                self.emit(SIGNAL('Data(QString)'),data)
+            #UDP packets
+            elif protocol == 17 :
+                u = iph_length + eth_length
+                udph_length = 8
+                udp_header = packet[u:u+8]
+
+                #now unpack them :)
+                udph = unpack('!HHHH' , udp_header)
+
+                source_port = udph[0]
+                dest_port = udph[1]
+                length = udph[2]
+                checksum = udph[3]
+
+                print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Length : ' + str(length) + ' Checksum : ' + str(checksum)
+
+                Source_Port = str(source_port)
+                Dest_Port = str(dest_port)
+                Length = str(length)
+                Checksum = str(checksum)
+
+                self.emit(SIGNAL('ICMP(QString,QString,QString,QString)'),Source_Port,Dest_Port,Length,Checksum)
+
+                h_size = eth_length + iph_length + udph_length
+                data_size = len(packet) - h_size
+
+                #get data from the packet
+                data = packet[h_size:]
+
+                print 'Data : ' + data
+
+            #some other IP packet like IGMP
+            else :
+                print 'Protocol other than TCP/UDP/ICMP'
+
+
+    def run(self):
+        qDebug('inside run')
+        self.main(self.dev)
+        self.exec_()
+
+    def BtnClicked_Slot(self,device):
+        self.dev = str(device)
+        qDebug('inside slot BtnClicked device = '+device)
